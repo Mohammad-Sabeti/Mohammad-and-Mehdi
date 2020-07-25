fcfb7b472f3d19e334679187b1e76ccaa1904bed
Mohammad-Sabeti <mohammadi.tik@gmail.com>
2020-07-25 22:13:40 +0430

    PrimNumberProject By Multi Threading (Multi Threading Example) Final Edit

--- MultiProcessing/setup.py
+++ MultiProcessing/setup.py
@@ -1,5 +1,5 @@
 from distutils.core import setup,Extension
-from    Cython.Build import cythonize
+from Cython.Build import cythonize
 
 ext=Extension(name="PrimNumberWithMultiThreading",sources=["PrimNumberWithMultiThreading.pyx"])
 setup(ext_modules=cythonize(ext))
--- MultiProcessing/test.py
+++ MultiProcessing/test.py
@@ -1,7 +1,7 @@
-import pnmt
+import PrimNumberWithMultiThreading
 import time
 n=int(input("Enter Number : "))
 t=int(input("Enter Theard Count : "))
 start_time=time.time()
-pnmt.PrimNumber(n,t)
+PrimNumberWithMultiThreading.PrimNumber(n,t)
 print("--- %s Seconds ---"%(time.time()-start_time))
